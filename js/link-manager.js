31732063b5017e0b8aafd038c654dc5ee999d9c8
Detroitaxle <samuel.detroitaxle@gmail.com>
2025-06-14 20:46:46 +0300

    Update link-manager.js

--- js/link-manager.js
+++ js/link-manager.js
@@ -1,1 +1,58 @@
-
+class LinkManager {
+    constructor() {
+        this.linkGroups = JSON.parse(localStorage.getItem('linkGroups')) || [];
+        this.editMode = false;
+        this.bulkEditMode = false;
+        this.init();
+    }
+    
+    init() {
+        this.renderLinkTree();
+        this.setupEventListeners();
+    }
+    
+    renderLinkTree() {
+        const container = document.getElementById('link-tree-container');
+        container.innerHTML = '';
+        
+        this.linkGroups.forEach(group => {
+            const groupElement = this.createGroupElement(group);
+            container.appendChild(groupElement);
+        });
+    }
+    
+    createGroupElement(group) {
+        // Create group with collapsible functionality
+        // Include edit/delete icons when in edit mode
+        // Render all links in the group
+    }
+    
+    addLink(groupId, linkData) {
+        // Add new link to specified group
+        this.saveToLocalStorage();
+    }
+    
+    addGroup(groupName) {
+        // Add new group
+        this.saveToLocalStorage();
+    }
+    
+    toggleEditMode() {
+        this.editMode = !this.editMode;
+        // Show/hide edit controls
+    }
+    
+    toggleBulkEditMode() {
+        this.bulkEditMode = !this.bulkEditMode;
+        // Show/hide checkboxes and bulk controls
+    }
+    
+    saveToLocalStorage() {
+        localStorage.setItem('linkGroups', JSON.stringify(this.linkGroups));
+        this.renderLinkTree();
+    }
+    
+    setupEventListeners() {
+        // Set up all button click handlers, etc.
+    }
+}
