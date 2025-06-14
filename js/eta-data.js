9b3ef36d8f1e5f6be01c7aa9f409b10c04ee909e
Detroitaxle <samuel.detroitaxle@gmail.com>
2025-06-14 22:05:40 +0300

    Update eta-data.js

--- js/eta-data.js
+++ js/eta-data.js
@@ -1,12 +1,12 @@
 const etaData = {
     texas: {
-        AL: 3,
-        AK: 7,
-        // ... all other states
+        AL: 3, AK: 7, AZ: 2, AR: 2, CA: 4,
+        CO: 3, CT: 5, DE: 5, FL: 3, GA: 3,
+        // Add all other states with sample ETAs
     },
     detroit: {
-        AL: 2,
-        AK: 6,
-        // ... all other states
+        AL: 2, AK: 6, AZ: 4, AR: 3, CA: 5,
+        CO: 3, CT: 2, DE: 2, FL: 4, GA: 2,
+        // Add all other states with sample ETAs
     }
 };
