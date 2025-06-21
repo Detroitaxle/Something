a5412b733bcf8035d8072055b6cfcbcb53f90876
Detroitaxle <samuel.detroitaxle@gmail.com>
2025-06-21 15:22:17 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,39 @@
-const CACHE_NAME = 'refund-calc-v1';
+const CACHE_NAME = 'refund-calc-v2';
 const ASSETS = [
-  '/Something/',
-  '/Something/index.html',
-  '/Something/app.webmanifest',
-  '/Something/icon-192.png',
-  '/Something/icon-512.png'
+    '/Something/',
+    '/Something/index.html',
+    '/Something/app.webmanifest',
+    '/Something/favicon.ico',
+    '/Something/favicon-16x16.png',
+    '/Something/favicon-32x32.png',
+    '/Something/styles.css'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(response => response || fetch(e.request))
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
